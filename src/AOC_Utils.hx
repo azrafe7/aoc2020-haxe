964816d7404f6acb284a2e32cf1d8aee34c15405
azrafe7 <azrafe7@gmail.com>
2020-12-16 23:48:37 +0100

    add boolToInt(), getMatches(), and getEnumValues() as util functions

--- src/AOC_Utils.hx
+++ src/AOC_Utils.hx
@@ -4,4 +4,19 @@ class AOC_Utils {
     inline public static function boolXor(a:Bool, b:Bool):Bool {
         return a && !b || (!a && b);
     }
+
+    inline public static function boolToInt(b:Bool):Int {
+        return b ? 1 : 0;
+    }
+
+    // https://code.haxe.org/category/beginner/regular-expressions.html
+    public static function getMatches(ereg:EReg, input:String, index:Int = 0):Array<String> {
+        var matches = [];
+        while (ereg.match(input)) {
+            matches.push(ereg.matched(index));
+            input = ereg.matchedRight();
+        }
+
+        return matches;
+    }
 }
