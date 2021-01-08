69d509aefd2f117b25ed5276a14df81370f22972
azrafe7 <azrafe7@gmail.com>
2021-01-08 12:49:09 +0100

    add php.hxml

--- src/Main.hx
+++ src/Main.hx
@@ -3,13 +3,13 @@ import solutions.*;
 class Main {
 
     static var dayClasses:Array<Class<BaseSolution>> = [
-        //Day1,
-        //Day2,
-        //Day3,
-        //Day4,
-        //Day5,
-        //Day6,
-        //Day7,
+        Day1,
+        Day2,
+        Day3,
+        Day4,
+        Day5,
+        Day6,
+        Day7,
         Day8,
     ];
 
